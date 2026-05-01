cc1d4e1b83f16cd55dd044531f3ef89f6a754e9c
ahmed yassine <ahmed.guerroudj@univ-constantine2.dz>
2026-05-01 22:00:24 +0100

    fix

--- services/mail/app/main.py
+++ services/mail/app/main.py
@@ -75,7 +75,7 @@ CORS_ORIGINS = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

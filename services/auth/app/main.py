4911ed8bb2c4f57c604b8a6c2de1b597c0ec91e9
ahmed yassine <ahmed.guerroudj@univ-constantine2.dz>
2026-05-01 17:24:42 +0100

    CORS_ORIGINS problem fix

--- services/auth/app/main.py
+++ services/auth/app/main.py
@@ -14,6 +14,8 @@ CORS_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://10.0.0.1:3000",
+    "http://10.8.0.1:3000",
 ]
 
 app.add_middleware(
